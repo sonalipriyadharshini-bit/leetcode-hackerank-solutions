795fe734328d9777d3ef164961a74e8eaeceb216
sonalipriyadharshini-bit <sonalipriyadharshini@gmail.com>
2026-03-26 12:29:54 +0530

    Implement conditional checks for weirdness based on N

---
+++ Java if - else.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(2 <= N && N<=5 ){
+            if(N % 2 == 0){
+                System.out.println("Not Weird");
+            }
+            else{
+            System.out.println("Weird");
+        }
+        }
+        else if(6 <= N && N<= 20){
+            if(N % 2 == 0){
+                System.out.println("Weird");
+            }
+            else{
+            System.out.println("Weird");
+        }
+        }
+        else if(N >= 20){
+            if(N % 2 == 0){
+                System.out.println("Not Weird");
+            }
+            else{
+            System.out.println("Weird");
+        }
+        }
+        
+        }
+        
+        
+}
+
