3569b8d56507287cd85ce739739cfe8d53b6b178
sonalipriyadharshini-bit <sonalipriyadharshini@gmail.com>
2026-03-31 19:08:23 +0530

    Implement solution for longest valid parentheses

---
+++ Longest valid paranthesis.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);  // base
+        
+        int maxLen = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+}
