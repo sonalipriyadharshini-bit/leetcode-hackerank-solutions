f345a584952a3227c6dff377f6f20b3852643d23
sonalipriyadharshini-bit <sonalipriyadharshini@gmail.com>
2026-04-22 21:44:58 +0530

    Add rand10 function using rand7
    Implement rand10 function using rand7 to generate random numbers between 1 and 10.

---
+++ Implement Rand 10 () using Rand 7().py
@@ -0,0 +1,8 @@
+class Solution:
+    def rand10(self):
+        while True:
+            row = rand7()
+            col = rand7()
+            idx = (row - 1) * 7 + col
+            if idx <= 40:
+                return 1 + (idx - 1) % 10
